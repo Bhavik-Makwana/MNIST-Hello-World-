d9e2b9ce5731918c40e7022cbab5defa1ed53189
bhavik-makwana <bhavikmakwana09@gmail.com>
2018-08-08 23:28:15 +0100

    code that actually ran for real lol

---
+++ MNIST.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+import tensorflow as tf
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+train_data = mnist.train.images  # Returns np.array
+train_labels = np.asarray(mnist.train.labels, dtype=np.int32)
+eval_data = mnist.test.images  # Returns np.array
+eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
+
+tf.logging.set_verbosity(old_v)
+
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+y = tf.nn.softmax(tf.matmul(x, W) + b)
+y_ = tf.placeholder(tf.float32, [None, 10])
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+for _ in range(1000):
+  batch_xs, batch_ys = mnist.train.next_batch(100)
+  sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+correct_prediction = tf.equal(tf.argmax(y,1), tf.argmax(y_,1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+
